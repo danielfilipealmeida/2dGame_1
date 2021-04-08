ead34bb4118ce8cfcdda17b486e2506f84ea5ea2
Daniel Almeida <danielf.almeida@gmail.com>
2021-04-08 11:44:09 +0100

    First commit

---
+++ Crystal.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+export var value = 1
+
+
+func _process (delta):
+ 
+	# rotate 90 degrees a second
+	rotation_degrees += 90 * delta
+
+
+func _on_Crystal_body_entered(body):
+	if body.name == "Player":
+		body.collect_coin(value)
+		queue_free()
